94bdadaad4388f6787771d4830351c6bb0ad2fd4
MRX-CYBER-404 <154981720+MRX-CYBER-404@users.noreply.github.com>
2023-12-27 18:04:00 +0600

    Create MRX-RUN.py

---
+++ MRX-RUN.py
@@ -0,0 +1,10 @@
+import os, platform, time, sys
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+print('\033[1;97m[\033[1;92m✓\033[1;97m]\033[1;92m CHECKING UPDATE...')          os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;97m[\033[1;92m✓\033[1;97m] \033[1;92m64 BIT FOUND.!!')             import MRX
+elif bit == '32bit':
+ print('\033[1;97m[\033[1;92m✓\033[1;97m] \033[1;92m 32 BIT NOT SUPPORTED.!!')
